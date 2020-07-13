c1d494181bcc446cd42a42a30fe79d719f45789f
thutgtz <thutboomz@gmail.com>
2020-07-13 17:54:05 +0700

    ....

--- lib/main.dart
+++ lib/main.dart
@@ -7,37 +7,8 @@ import 'package:http/http.dart' as http;
 import 'package:travel_sharing/ChatFile/chatPage.dart';
 import 'package:travel_sharing/Pages/home.dart';
 import 'Pages/loginPage.dart';
-
 import 'Pages/map.dart';
 import 'Pages/DirectionsProvider.dart';
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    Iterable list = json.decode(response.body);
-    return list.map((model) => Album.fromJson(model)).toList();
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
-  }
-}
-
-class Album {
-  final String id;
-  final String name;
-  final int age;
-
-  Album({this.id, this.name, this.age});
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      id: json['_id'],
-      name: json['name'],
-      age: json['age']
-    );
-  }
-}
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
@@ -75,63 +46,11 @@ class MyApp extends StatelessWidget {
           color: Colors.white
           )
         ),
-        home: LoginPage(),
+        home: CreateRoute(),
 //      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      )
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  Future<List<Album>> futureAlbum;
-
-  @override
-  void initState() {
-    super.initState();
-    futureAlbum = fetchAlbum();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: FutureBuilder<List<Album>>(
-          future: futureAlbum,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (context, index) {
-                  Album project = snapshot.data[index];
-                  return Column(
-                    children: <Widget>[
-                      SizedBox(height: 16.0),
-                      Text(project.name),
-                      SizedBox(height: 16.0),
-                    ],
-                  );
-                },
-              );
-            } else if (snapshot.hasError) {
-              return Text("${snapshot.error}");
-            }
-
-            // By default, show a loading spinner.
-            return CircularProgressIndicator();
-          },
-        ),
-      ),
-    );
-  }
-}
